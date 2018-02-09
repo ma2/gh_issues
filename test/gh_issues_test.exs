3997db118b57942bcfced39fca6c5867f8904f50
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-09 20:49:31 +0900

    test: parse_argsのテストを追加

--- test/gh_issues_test.exs
+++ test/gh_issues_test.exs
@@ -2,7 +2,18 @@ defmodule GhIssuesTest do
   use ExUnit.Case
   doctest GhIssues
 
-  test "greets the world" do
-    assert GhIssues.hello() == :world
+  import GhIssues.Cli, only: [parse_args: 1]
+
+  test "引数が-hか--helpのときは:helpが返ること" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "引数が3つあったら、その3つが返ること" do
+    assert(parse_args(["user", "project", "99"]) == {"user", "project", 99})
+  end
+
+  test "引数が2つだったら、最後に@default_countが追加されて返ること" do
+    assert(parse_args(["user", "project"]) == {"user", "project", 4})
   end
 end
