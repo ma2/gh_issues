7b008c8f30fdb1488e15fe6eb6dcb9b0122e0215
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-09 20:49:02 +0900

    feat: コマンド行を解釈するコードを実装

---
+++ lib/gh_issues/cli.ex
@@ -0,0 +1,27 @@
+defmodule GhIssues.Cli do
+  @default_count 4  # イコールを使わないことに注意
+
+  @moduledoc """
+  コマンドラインを解析する
+  指定されたGitHubプロジェクトのissueの最新の _n_ 個を表形式で表示する
+  """
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` が-hか--helpだった場合には:helpを返す
+  そうでなければGitHubのユーザ名、プロジェクト名、issueの取得個数（オプション）である
+  `{ user, project, count }`タプルを返す
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
+      {_, [user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
+  end
+end
