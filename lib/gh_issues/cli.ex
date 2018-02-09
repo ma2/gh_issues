13354f78c12902e95a8dda88d4af7425cc5eb26e
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-09 22:08:40 +0900

    refactor: フォーマット

--- lib/gh_issues/cli.ex
+++ lib/gh_issues/cli.ex
@@ -15,12 +15,20 @@ defmodule GhIssues.Cli do
   `{ user, project, count }`タプルを返す
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    parse = OptionParser.parse(
+      argv,
+      switches: [
+        help: :boolean
+      ],
+      aliases: [
+        h: :help
+      ]
+    )
 
     case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
-      {_, [user, project], _} -> {user, project, @default_count}
+      { [ help: true ], _, _ } -> :help
+      { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
+      { _, [ user, project ], _ } -> { user, project, @default_count }
       _ -> :help
     end
   end
