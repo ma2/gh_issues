6833833827a867ce42565084720dcacfa8ff0e05
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-09 22:48:57 +0900

    chore: httpクライアントとjsonデコーダを追加

--- mix.exs
+++ mix.exs
@@ -11,16 +11,19 @@ defmodule GhIssues.MixProject do
     ]
   end
 
+  # ここで起動時に実行されるアプリケーションを指定できる
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [ :logger, :httpoison ]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      { :httpoison, "~>1.0.0" },
+      { :poison, "~>3.1.0" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
