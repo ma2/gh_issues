ca2bbb67a37fca2bc0eeb7a41f22ea4dd547bc40
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-09 20:07:59 +0900

    first commit

---
+++ lib/gh_issues.ex
@@ -0,0 +1,18 @@
+defmodule GhIssues do
+  @moduledoc """
+  Documentation for GhIssues.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GhIssues.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gh_issues_test.exs
@@ -0,0 +1,8 @@
+defmodule GhIssuesTest do
+  use ExUnit.Case
+  doctest GhIssues
+
+  test "greets the world" do
+    assert GhIssues.hello() == :world
+  end
+end
