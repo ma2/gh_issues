f209c91be65a7648f177ac02a19d66dad488bb54
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-11 14:56:46 +0900

    feat: 表形式に整形する関数を実装

--- lib/gh_issues/cli.ex
+++ lib/gh_issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule GhIssues.Cli do
+  import GhIssues.TableFormatter, only: [ print_table_for_columns: 2 ]
   @default_count 4  # イコールを使わないことに注意
 
   @moduledoc """
@@ -24,6 +25,7 @@ defmodule GhIssues.Cli do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns([ "number", "created_at", "title" ])
   end
 
   @doc """
