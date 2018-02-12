7b6b45ccbdb9832ae54ad7a9882b91fcfe6e7570
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2018-02-12 21:41:56 +0900

    feat: Loggerを追加してログをとるようにした

--- lib/gh_issues/issues.ex
+++ lib/gh_issues/issues.ex
@@ -1,14 +1,19 @@
 defmodule GhIssues.Issues do
+  require Logger
+
   @user_agent [ { "User-agent", "Elixir keiichi.2600.matsunaga@gmail.com" } ]
   @github_url Application.get_env(:gh_issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
   end
 
   def issues_url(user, project) do
+    Logger.info "Successful Response"
+    Logger.debug fn -> inspect(body) end
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
@@ -16,7 +21,8 @@ defmodule GhIssues.Issues do
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _, %{ status_code: _, body: body } }) do
+  def handle_response({ _, %{ status_code: status, body: body } }) do
+    Logger.error "Error #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
 
